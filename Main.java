7e6dd0b8900e2f65c4ffc71f9261985e3f6d8c4d
briankaboyi <briankaboyi0@gmail.com>
2023-11-24 20:09:53 +0300

    operators

--- Main.java
+++ Main.java
@@ -1,26 +1,93 @@
 public class Main{
    
     public static void main(String args[]){
+/* */
+// Find fun = new Find();
+// int[] numArray = {2,4,6,41};
+// int[] twoArray;
+// twoArray = new int[7];
+// twoArray[0]=71;
+// twoArray[1]=72;
+// twoArray[2]=73;
+// twoArray[3]=74;
+// twoArray[4]=75;
+// twoArray[5]=76;
+// twoArray[6]=77;
+// System.out.println(twoArray[6]);
+// fun.findValue(3,numArray);
+// fun.findValue(4, twoArray);
 
-Find fun = new Find();
-int[] numArray = {2,4,6,41};
-int[] twoArray;
-twoArray = new int[7];
-twoArray[0]=71;
-twoArray[1]=72;
-twoArray[2]=73;
-twoArray[3]=74;
-twoArray[4]=75;
-twoArray[5]=76;
-twoArray[6]=77;
-System.out.println(twoArray[6]);
-fun.findValue(3,numArray);
-fun.findValue(4, twoArray);
+ ////ARITHMETIC OPERATORS////////////////////////////////////////////
+//  int result = 1+2;
+//  System.out.println("1+2= " + result);
+//  int original_result = result;
+
+//  result = result -1;
+//  //result is now 2
+//  System.out.println(original_result + " - 1 = " + result);
+//  original_result = result;
+
+//  result = result * 2;
+//  //result is now 4
+//  System.out.println(original_result + " * 2 = " + result);
+//  original_result = result;
+
+//  result = result / 2;
+//  //result is now 2
+//  System.out.println(original_result + " / 2 = " + result);
+//  original_result = result;
+
+//  result = result + 8;
+//  //result is now 10
+//  System.out.println(original_result + " + 8 = " + result);
+//  original_result = result;
+
+//  result = result % 7;
+//     //result is now 3
+//     System.out.println(original_result + " % 7 = " + result);
+
+    /////////////////////URNARY OPERATORS/////////////////////////////
+/*
+ ++ increment operator, increases the value by 1 
+ -- decrement operator, decrease the value by 1 
+ ! logical operator, reverses the value of a boolean
+ 
+ */
+int result = +1;
+// result is now 1
+System.out.println(result);
+
+result--;
+// result is now 0
+
+result++;
+// result is now 1
+
+result =- result;
+// result is now -1
+System.out.println(result);
+
+boolean success = false;
+// false
+System.out.println(success);
+// true
+System.out.println(!success);
+/////////////////
+int i =3;
+++i;
+// prints 4
+System.out.println(i);
+//prints 5
+System.out.println(i);
+//prints 6
+System.out.println(++i);
     }
+   
+
 }
 
-class Find{
- void findValue(int a,int[] b){
-        System.out.println(b[a]);
-    }
-}
+// class Find{
+//  void findValue(int a,int[] b){
+//         System.out.println(b[a]);
+//     }
+// }
